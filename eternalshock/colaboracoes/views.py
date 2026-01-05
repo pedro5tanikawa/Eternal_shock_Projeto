9f4ad362ee9200b0429f3002582f1e59646fac51
pedro5tanikawa <phlkawa@gmail.com>
2026-01-04 23:38:57 -0300

    nem pra frente nem pra tras

--- eternalshock/colaboracoes/views.py
+++ eternalshock/colaboracoes/views.py
@@ -4,7 +4,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def colaboracoes(request):
+    contexto = {
+        'title' : 'Eternal Shock | Colaborações'
+    }
     return render(
         request,
-        'colaboracoes/index.html'
+        'colaboracoes/index.html',
+        contexto,
     )
